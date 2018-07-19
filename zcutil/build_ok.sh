31d5f57dc4cf037891ace27640c84bf183f49536
wangwenfeng01 <wangwenfeng01@baidu.com>
2018-07-19 16:35:13 +0800

    build ok

---
+++ zcutil/build_ok.sh
@@ -0,0 +1,14 @@
+#1
+./build.sh
+
+#2
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar xzvf LATEST.tar.gz
+cd libsodium-stable
+./configure --disable-pie
+make
+cp -f ./src/libsodium/.libs/libsodium.a ../../depends/x86_64-unknown-linux-gnu/lib/libsodium.a
+
+#3
+cd ../../src
+make -f Makefile_src_centos
