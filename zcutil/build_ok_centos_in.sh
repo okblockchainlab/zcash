2ab5a633c392bf90b96cd4563efd4b6b15ab2543
wenfeng.wang@okcoin.com <wenfeng.wang@okcoin.com>
2018-09-26 19:42:09 +0800

    build test

--- zcutil/build_ok_centos_in.sh
+++ zcutil/build_ok_centos_in.sh
@@ -117,4 +117,5 @@ HOST="$HOST" BUILD="$BUILD" NO_PROTON="$PROTON_ARG" "$MAKE" "$@" -C ./depends/ V
 ./autogen.sh
 CC="$CC" CXX="$CXX" ./configure --prefix="${PREFIX}" --host="$HOST" --build="$BUILD" "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG" $CONFIGURE_FLAGS --enable-werror CXXFLAGS='-g'
 sed -i "s/PIE_FLAGS = -fPIE/PIE_FLAGS = -fPIC/g" src/Makefile
+sed -i "s/ -pie/ /g" src/Makefile
 "$MAKE" "$@" V=1
