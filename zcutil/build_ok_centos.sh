c9cf84cde8419bcefe2197378cbe6b64be588557
wenfeng.wang@okcoin.com <wenfeng.wang@okcoin.com>
2018-09-27 15:17:16 +0800

    make file end

--- zcutil/build_ok_centos.sh
+++ zcutil/build_ok_centos.sh
@@ -10,6 +10,15 @@ make
 cp -f ./src/libsodium/.libs/libsodium.a ../../depends/x86_64-unknown-linux-gnu/lib/libsodium.a
 
 #3
+cd ..
+wget https://gmplib.org/download/gmp/gmp-6.1.1.tar.bz2
+tar -jxvf gmp-6.1.1.tar.bz2
+cd gmp-6.1.1
+./configure --with-pic
+make
+cp -f ./.libs/libgmp.a ../../depends/x86_64-unknown-linux-gnu/lib/libgmp.a
+
+#4
 cd ../../src
 rm -f zcash-cli-ok.so
 rm -f bitcoin-cli-ok.o
