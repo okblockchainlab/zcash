2cfa6f8a2676aef54e46e20cae7b8937a9e8e454
wenfeng.wang@okcoin.com <wenfeng.wang@okcoin.com>
2018-09-28 16:57:55 +0800

    build_ok_centos.sh

--- zcutil/build_ok_centos.sh
+++ zcutil/build_ok_centos.sh
@@ -2,24 +2,29 @@
 #./build_ok_centos_in.sh
 
 #2
-#wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
-#tar -xzvf LATEST.tar.gz
-#cd libsodium-stable
-#./configure --disable-pie
-#make
-#cp -f ./src/libsodium/.libs/libsodium.a ../../depends/x86_64-unknown-linux-gnu/lib/libsodium.a
-#cd ..
-
+sodiumPath="libsodium-stable/"
+if [ ! -d "$sodiumPath" ]; then
+    wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+    tar -xzvf LATEST.tar.gz
+    cd libsodium-stable
+    ./configure --disable-pie
+    make
+    cp -f ./src/libsodium/.libs/libsodium.a ../../depends/x86_64-unknown-linux-gnu/lib/libsodium.a
+    cd ..
+fi
 
 #3
-#wget https://gmplib.org/download/gmp/gmp-6.1.1.tar.bz2
-#tar -jxvf gmp-6.1.1.tar.bz2
-#cd gmp-6.1.1
-#./configure --with-pic
-#make
-#cp -f ./.libs/libgmp.a ../../depends/x86_64-unknown-linux-gnu/lib/libgmp.a
-#cd ../../src
+gmpPath="/home/zcash/zcutil/gmp-6.1.1/"
+if [ ! -d "$gmpPath" ]; then
+    wget https://gmplib.org/download/gmp/gmp-6.1.1.tar.bz2
+    tar -jxvf gmp-6.1.1.tar.bz2
+    cd gmp-6.1.1
+    ./configure --with-pic
+    make
+    cp -f ./.libs/libgmp.a ../../depends/x86_64-unknown-linux-gnu/lib/libgmp.a
 
+fi
 
 #4
+cd ../../src
 make -f Makefile_src_centos
