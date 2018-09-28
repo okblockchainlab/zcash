6b818cb08ae45e5db4bc2eced118b53e515dbed0
wenfeng.wang@okcoin.com <wenfeng.wang@okcoin.com>
2018-09-28 19:33:16 +0800

    serverp

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -353,7 +353,7 @@ static const CRPCCommand vRPCCommands[] =
     { "wallet",             "getaccount",             &getaccount,             true  },
     { "wallet",             "getaddressesbyaccount",  &getaddressesbyaccount,  true  },
     { "wallet",             "getbalance",             &getbalance,             false },
-    { "wallet",             "getnewaddress",          &getnewagetnewaddressddress,          true  },
+    { "wallet",             "getnewaddress",          &getnewaddress,          true  },
     { "wallet",             "getrawchangeaddress",    &getrawchangeaddress,    true  },
     { "wallet",             "getreceivedbyaccount",   &getreceivedbyaccount,   false },
     { "wallet",             "getreceivedbyaddress",   &getreceivedbyaddress,   false },
